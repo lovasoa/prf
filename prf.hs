afe1196ada542412e17fb6b2d77d9ab1aa2720a7
Ophir LOJKINE <pere.jobs@gmail.com>
2016-04-07 14:35:49 +0300

    Add an optimizer to execute functions faster

--- prf.hs
+++ prf.hs
@@ -60,25 +60,97 @@ dependOnArg 0 (Recurse _ (P 2 _)) = False
 dependOnArg 0 g = True
 dependOnArg k (Recurse f g) = dependOnArg (k-1) f && dependOnArg (k+1) g
 
+data Optimized = OConst Int | OAdd | OSub | OMul | OIf | OAddNullArgs Int Optimized
+                  | ORec Optimized Optimized
+                  | OComp Optimized [Optimized] | OProj Int Int
+                  deriving (Eq, Show)
+
+addNullArg :: Optimized -> Optimized
+addNullArg (OProj i n) = OProj (i+1) (n+1)
+addNullArg (OAddNullArgs n o) = OAddNullArgs (n+1) o
+addNullArg o = OAddNullArgs 1 o
+
+-- optimize composition
+mkComp :: Optimized -> [Optimized] -> Optimized
+-- f(a(g,h), b(g,h)) = (f(a,b))(g,h)
+mkComp (OComp f ab) gh = OComp f $ map (flip mkComp gh) ab
+mkComp (OConst c) gs = OConst c
+mkComp (OProj i n) gs = gs !! (i-1)
+mkComp f gs = OComp f gs
+
+optimize :: Prf -> Optimized
+-- Use addition instead of a series of S
+optimize (Compose S [f]) =
+  case optimize f of
+    OConst i -> OConst (i+1)
+    OComp OAdd ((OConst c):others) -> OComp OAdd ((OConst (c+1)):others)
+    OComp OAdd others -> OComp OAdd ((OConst 1):others)
+    other -> OComp OAdd [(OConst 1), other]
+-- When composing with a projection, delete the projection
+optimize (Compose (P i n) gs) = optimize (gs !! (i-1))
+-- Detect constant null function defined as recursive function
+optimize (Recurse C0 (P 2 n)) = OConst 0
+-- Optimize addition
+optimize (Recurse (P 1 1) (Compose S [P 2 3])) = OAdd
+optimize (Recurse f (Compose S [P 2 n])) = OComp OAdd [OProj 1 (n-1), addNullArg (optimize f)]
+-- Optimize precedent
+optimize (Recurse C0 (P 1 2)) = OComp OSub [OConst 1, OProj 1 1]
+optimize (Recurse f g) | not $ dependOnArg 1 g = let
+  n = 1 + arity f
+  gcomp =  (OComp OSub [OConst 1, OProj 1 n]):(OConst 0):[OProj i n | i <- [2..n]]
+  gopt = mkComp (optimize g) gcomp
+  fopt = addNullArg $ optimize f
+  in OComp OIf [OProj 1 n, gopt, fopt]
+-- Optimize multiplication
+optimize (Recurse f g) = case (optimize f, optimize g) of
+  (OConst 0, OComp OAdd [OProj 2 3,OProj 3 3]) -> OMul
+  (OProj 1 1, OComp OSub [OConst 1, OProj 2 3]) -> OSub
+  (optf, optg) -> ORec optf optg
+optimize C0 = OConst 0
+optimize S  = OComp OAdd [OConst 1, OProj 1 1]
+optimize (P i n) = OProj i n
+optimize (Compose f g) = mkComp (optimize f) (map optimize g)
+
+-- Optimize a function and execute it
 exec :: Prf -> [Int] -> Int
-exec C0 _ = 0
-exec S [n] = n `seq` n + 1
-exec (P i n) args | n == length args = args `seq` args !! (i - 1)
-exec (Compose f gs) args = exec f $! map (flip exec args) gs
-exec (Recurse f g)  (n:args) = let
-                                  folder res i = exec g (i:res:args)
-                                  init = exec f args
+exec = execopt . optimize
+
+-- Execute optimized function
+execopt :: Optimized -> [Int] -> Int
+execopt (OConst n) _ = n
+execopt OAdd ints = sum ints
+execopt OSub [a,b] = if b > a then b-a else 0
+execopt OMul ints = product ints
+execopt (OProj i n) ints = ints !! (i-1)
+execopt (OComp f gs) ints = execopt f $ map (flip execopt ints) gs
+execopt (ORec f g) (n:ints) = let
+  init = execopt f ints
+  folder res i = execopt g (i:res:ints)
+  in foldl folder init [0..(n-1)]
+execopt (OAddNullArgs n f) ints = execopt f ((replicate n 0)++ints)
+execopt OIf [a,b,c] = if a/=0 then b else c
+execopt other ints = error ("unimplemented execution of " ++ (show other) ++ (show ints))
+
+-- Execute a function without optimizing it
+execprf :: Prf -> [Int] -> Int
+execprf C0 _ = 0
+execprf S [n] = n `seq` n + 1
+execprf (P i n) args | n == length args = args `seq` args !! (i - 1)
+execprf (Compose f gs) args = execprf f $! map (flip execprf args) gs
+execprf (Recurse f g)  (n:args) = let
+                                  folder res i = execprf g (i:res:args)
+                                  init = execprf f args
                                in init `seq` foldl folder init [0..(n-1)]
-exec prog args = traceShow (prog,args) (error "Invalid arguments for this Program")
+execprf prog args = traceShow (prog,args) (error "Invalid arguments for this Program")
 
 multiExec :: Prf -> [[Int]] -> [Maybe Int]
 multiExec pgm intss =
   let
     ar = arity pgm
-    executor = exec pgm
-    mapper ints | length ints == ar = Just (executor ints)
+    optimized = optimize pgm
+    mapper ints | length ints == ar = Just (execopt optimized ints)
     mapper ints | otherwise         = Nothing
-  in map mapper intss
+  in optimized `seq` map mapper intss
 
 fromParse :: PrfParse.Prf -> Prf
 fromParse f = case f of
